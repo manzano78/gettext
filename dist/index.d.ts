bb7b5ccca0f58a3fa0149e27b89efa43b31816af
Mikaël ANZANO <mikael.anzano@gmail.com>
2020-03-02 09:57:53 +0100

    Initial commit

---
+++ dist/index.d.ts
@@ -0,0 +1,13 @@
+import React from 'react';
+import { HTMLTranslateProps, TranslateProps, TranslatorContext, TranslatorProviderProps } from './types';
+declare const TranslatorContext: React.Context<TranslatorContext>;
+export declare function TranslatorProvider(props: TranslatorProviderProps): JSX.Element;
+export declare function Translate(props: TranslateProps): JSX.Element;
+export declare function HTMLTranslate<T extends keyof JSX.IntrinsicElements>(props: HTMLTranslateProps<T>): React.DOMElement<Pick<HTMLTranslateProps<T>, Exclude<keyof JSX.IntrinsicElements[T], "count" | "msgctxt" | "msgid" | "msgidPlural" | "params" | "comments" | "renderAs">> & {
+    children: null;
+    dangerouslySetInnerHTML: {
+        __html: string;
+    };
+}, SVGViewElement>;
+export declare function useTranslator(): TranslatorContext;
+export {};
